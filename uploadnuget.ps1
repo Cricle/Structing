67963dd22d3c907a6a8599104b63d0d3d6a9094d
HCricle <none>
2022-01-04 22:58:47 +0800

    update to 1.2.0

--- uploadnuget.ps1
+++ uploadnuget.ps1
@@ -1,5 +1,5 @@
-$version="1.1.1"
-$accessToken="oy2n3klc2wrxtmghryrarvgns3pqx2we3bff5pnvdagkty"
+$version="1.2.0"
+$accessToken="oy2oved6ok7dmek4boka73v7ahlqyu3u7a5scjrcavx7g4"
 $paths=@("Structing","Structing.Core","Structing.Outsize","Structing.NetCore");
 for($x=0;$x -lt $paths.length; $x=$x+1)
 {
